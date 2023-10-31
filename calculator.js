e4cc7317212cd4560f6feefbf4b52b6ce121631b
Mudit-morningstar <gupta.mudit2299@gmail.com>
2023-11-01 01:23:12 +0530

    init commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let num1, num2, operator;
+
+const add = (a,b) => {
+    return a+b;
+}
+
+const subtract = (a,b) => {
+    return a-b;
+}
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
+const divide = (a,b) => {
+
+    return b!=0 ? a/b : "#ERR";
+}
