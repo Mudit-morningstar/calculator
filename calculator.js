12e7c43c4376a9ac03298dbbe4b90ecce11d7a00
Mudit-morningstar <gupta.mudit2299@gmail.com>
2023-11-01 01:32:55 +0530

    added operate function

--- calculator.js
+++ calculator.js
@@ -16,3 +16,20 @@ const divide = (a,b) => {
 
     return b!=0 ? a/b : "#ERR";
 }
+
+const operate = (a, b, operation) => {
+    switch(operation){
+        case "add": 
+            add(a,b);
+            break;
+        case "subtract":
+            subtract(a,b);
+            break;
+        case "multiply":
+            multiply(a,b);
+            break;
+        case "divide":
+            divide(a,b);
+            break;
+    }
+}
